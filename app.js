a6d1f364fe33a982116c3bda61a0e50d7ceac6f4
MontanoDiego <diego.e2.montano@gmail.com>
2022-11-02 12:18:00 -0700

    all dropdowns are functional

--- app.js
+++ app.js
@@ -11,49 +11,54 @@ const catchphraseInput = document.getElementById('catchphrase-input');
 const catchphraseButton = document.getElementById('catchphrase-button');
 
 // set state for how many times the user changes the head, middle, and bottom
+let headCounter = 0;
+let middleCounter = 0;
+let bottomCounter = 0;
+
 // set state for all of the character's catchphrases
+let catchphrases = [];
 
-headDropdown.addEventListener('change', () => {
+headDropdown.addEventListener('change', (e) => {
     // get the value of the head dropdown
-
+    const value = e.target.value;
     // increment the head change count state
-    
+    headCounter++;
     // update the dom for the head (use style.backgroundImage on the headEl div instead of trying to set the .src -- it's NOT an img tag!)
-
+    headEl.style.backgroundImage = `url('./assets/${value}-head.png')`;
     // update the stats to show the new count (call displayStats() to do this work)
 });
 
 
-middleDropdown.addEventListener('change', () => {
+middleDropdown.addEventListener('change', (e) => {
     // get the value of the middle dropdown
-
+    const value = e.target.value;
     // increment the middle change count state
-    
+    middleCounter++;
     // update the dom for the middle (NOTE: use style.backgroundImage on the middleEl div instead of trying to set the .src -- it's NOT an img tag!)
-
+    middleEl.style.backgroundImage = `url('./assets/${value}-middle.png')`;
     // update the stats to show the new count (call displayStats() to do this work)
 });
 
 
-bottomDropdown.addEventListener('change', () => {
+bottomDropdown.addEventListener('change', (e) => {
     // get the value of the bottom dropdown
-
+    const value = e.target.value;
     // increment the bottom change count state
-    
+    bottomCounter++;
     // update the dom for the bottom (NOTE use style.backgroundImage on the bottomEl div instead of trying to set the .src -- it's NOT an img tag!)
-
+    bottomEl.style.backgroundImage = `url('./assets/${value}-pants.png')`;
     // update the stats to show the new count (call displayStats() to do this work)
 });
 
 catchphraseButton.addEventListener('click', () => {
     // get the value of the catchphrase input
-    
+    let catchphrase = catchphraseInput.value;
     // push the new catchphrase to the catchphrase array in state
-
+    catchphrases.push(catchphrase);
     // clear out the form input's value so it's empty to the user
-   
+    catchphraseInput.value = '';
     // update the dom to show the new catchphrases (refactor to/call displayCatchphrases to do this work)
-
+    
 });
 
 function displayStats() {
@@ -64,7 +69,7 @@ function displayCatchphrases() {
     // clear out the DOM for the currently displayed catchphrases
 
     // loop through each catchphrase in state
-   
+
     // and for each catchphrase
     
     // create an HTML element with the catchphrase as its text content
